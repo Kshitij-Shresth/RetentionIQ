e923001ea07e95b2f62b2f84f2ca80053f50fab3
Kshitij Shresth <kshitijshresth@icloud.com>
2024-09-08 12:04:58 +0300

    Update data.py
    Performing one hot encoding on the dataset

--- data.py
+++ data.py
@@ -1,1 +1,8 @@
+data = pd.read_csv('employee.csv')
+
+#One hot encoding 
+data['Attrition'] = data['Attrition'].apply(lambda x: 1 if x == 'Yes' else 0)
+data['Gender'] = data['Gender'].apply(lambda x: 1 if x == 'Male' else 0)
+data['Over18'] = data['Over18'].apply(lambda x: 1 if x == 'Y' else 0)
+data['OverTime'] = data['OverTime'].apply(lambda x: 1 if x == 'Yes' else 0)
 
