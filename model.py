e8d4a80abc6cb20d506efe4ffe055a634021582f
Kshitij Shresth <kshitijshresth@icloud.com>
2024-09-09 11:46:43 +0300

    Update model.py
    Training

--- model.py
+++ model.py
@@ -1,1 +1,8 @@
+from imports import RandomForestClassifier, train_test_split, classification_report, accuracy_score
+import pandas as pd
+import numpy as np
 
+def train_model(X_train, y_train):
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    return model
