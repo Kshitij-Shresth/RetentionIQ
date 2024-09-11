ea7b27dac9c752340dbb5b5cb21822cc2e256d8b
Kshitij Shresth <kshitijshresth@icloud.com>
2024-09-11 11:27:33 +0300

    Update features.py

--- features.py
+++ features.py
@@ -5,3 +5,11 @@ def plot_top_features(pipeline):
                              pipeline.named_steps['preprocessor'].transformers_[1][1].get_feature_names_out().tolist())
     importance_df = pd.DataFrame({'Feature': feature_names, 'Importance': feature_importances})
     importance_df = importance_df.sort_values(by='Importance', ascending=False)
+
+#Feature importance
+    plt.figure(figsize=(10, 8))
+    sns.barplot(x='Importance', y='Feature', data=importance_df.head(10))
+    plt.title('Top 10 Important Features')
+    plt.xlabel('Importance')
+    plt.ylabel('Feature')
+    plt.show()
